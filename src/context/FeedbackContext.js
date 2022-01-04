20975fa82de572313bd004355a4ca6cdd8a7f2f6
ahernandezjr <76761720+ahernandezjr@users.noreply.github.com>
2022-01-03 23:30:41 -0600

    Fixed bug where add function remained unusable after editing

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -29,6 +29,10 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback(
       feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)),
     )
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    })
   }
 
   // Set Item to be updated
