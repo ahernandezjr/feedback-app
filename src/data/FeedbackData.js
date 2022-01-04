c92af58a4afb00a14a66aa8f3d8a4a157679c30c
ahernandezjr <76761720+ahernandezjr@users.noreply.github.com>
2022-01-03 23:20:27 -0600

    Section 6 Complete: Added the context API and  useContext hooks

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -9,13 +9,13 @@ const FeedbackData = [
     id: 2,
     rating: 9,
     text:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      'Borem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
     id: 3,
     rating: 8,
     text:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      'Porem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 ]
 
